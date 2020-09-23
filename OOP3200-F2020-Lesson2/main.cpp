dec5195afea8007266b135b87ec6f611c61d0c89
Gabriel Dietrich <gabrieldietrich@hotmail.com>
2020-09-23 10:37:25 -0400

    Midway through class

--- OOP3200-F2020-Lesson2/main.cpp
+++ OOP3200-F2020-Lesson2/main.cpp
@@ -3,74 +3,20 @@
  *	In this example program, did the following:
  *		- Examined a simple class with 'instance' data-members, a constructor, accessors, mutators.
  *
- *	@author		<YOUR-NAME>
- *	@studentID  <YOUR STUDENT ID>
+ *	@author		Gabriel Dietrich
+ *	@studentID  100764733
 */
 
 #include <iostream> 		// cin, cout
 #include <iomanip>			// fixed, setprecision()
-#include <sstream> 			// stringstream
-#include <cmath>			// sqrt()
-#include <limits>			// INT_MAX
 #include <stdexcept>		// out_of_range
+
+#include "CartesianPoint.h"
 #include "MyConsoleInput.h" // ConsoleInput::ReadInteger()
 
 using namespace std;
 
-// class declaration section
-class CartesianPoint
-{
-public:
 
-	/* Constructor: Used to initialize objects
-	*	- always the same name as the class
-	*	- never have a return type
-	*	- called automatically when an obj is instantiated
-	*	- should set values for each member variable
-	*/
-	CartesianPoint(int x = 1, int y = 1);
-
-	// --------------------------------------------------------------------------------
-	/* Accessors: Used to query the state of the object
-	*	- never modifies the object
-	*	- should specify const at the end of the prototype/header
-	*/
-
-	// get x
-	int GetX();
-	
-	// get y
-	int GetY();
-	
-	// -------------------------------------------------------------------------------
-	/* Mutator(s): Used to change the state of the object
-	*	- should contain logic to ensure object remains in a valid state.
-	*	- typically sets a member variable to a parameter
-	*/
-
-	// set x
-	void SetX(int x);
-	
-	// set y,
-	void SetY(int y);
-	
-	// set point (both x and y)
-	void SetPoint(int x, int y);
-
-
-	// get the distance between this point and a second point
-	double GetDistanceTo(CartesianPoint pointTo) const;
-
-	// convert the obj to a string
-	string ToString() const;
-
-
-private:
-	// private data members for the dimensions of the point
-	int myX; // x-axis (horizontal) value
-	int myY;  // y-axis (vertical) value
-
-};
 
 // main() function
 int main()
@@ -78,13 +24,13 @@ int main()
 	// Declarations
 	// a point called 'origin', initialized to 0, 0
 	CartesianPoint origin = CartesianPoint(0, 0);
-	
+
 	// a point called 'destination'
 	CartesianPoint destination;
 
 	int inputX; // temporary input variable for x
 	int inputY; // temporary input variable for y
-	
+
 	// to store the distance between two points
 	double distance;
 
@@ -117,7 +63,7 @@ int main()
 			<< distance << ". " << endl;
 	}
 
-	catch (exception& ex)
+	catch (exception & ex)
 	{
 		cerr << ex.what() << " Ending the program." << endl;
 	}
@@ -127,71 +73,3 @@ int main()
 	return 0;
 } // end of main()
 
-// Class definition section
-
-// constructor for CartesianPoint
-CartesianPoint::CartesianPoint(int x, int y)
-{
-	SetPoint(x, y);
-}
-
-void CartesianPoint::SetPoint(int x, int y)
-{
-	SetX(x);
-	SetY(y);
-}
-
-void CartesianPoint::SetX(int x)
-{
-	myX = x;
-}
-
-void CartesianPoint::SetY(int y)
-{
-	myY = y;
-}
-
-int CartesianPoint::GetX()
-{
-	return myX;
-}
-
-int CartesianPoint::GetY()
-{
-	return myY;
-}
-
-
-/** GetDistanceTo Method for CartesianPoint class
-*	Determines the distance between this point and a second point.
-*	@param	pointTo: CartesianPoint
-*	@return	the distance as a double
-*/
-double CartesianPoint::GetDistanceTo(CartesianPoint pointTo) const
-{
-	// difference between x values
-	int xDelta = pointTo.myX - myX;
-
-	// difference between y values
-	int yDelta = pointTo.myY - myY;
-
-	// return the formula (based on Pythagorean theorem)
-	return sqrt((xDelta * xDelta) + (yDelta * yDelta));
-}
-
-/** ToString Method for CartesianPoint class
-*	Converts the obj to a string.
-*	@return	the obj state as a string
-*/
-
-string CartesianPoint::ToString() const
-{
-	// declare a stringstream object
-	stringstream strOut;
-	
-	// build the string
-	strOut << "(" << myX << ", " << myY << ")";
-	
-	// return the string
-	return strOut.str();
-}
